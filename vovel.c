bb1708a35a0f24ef3c3745721b350375433e168b
selvakm75 <30317749+selvakm75@users.noreply.github.com>
2017-07-21 17:59:57 +0530

    Create vovel.c

---
+++ vovel.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+char ch;
+printf("Enter any character: ");
+scanf("%c", &ch);
+if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+{
+printf("%c is VOWEL.", ch);
+}
+else if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+{
+printf("%c is CONSONANT.", ch);
+}
+return 0;
+}
+
