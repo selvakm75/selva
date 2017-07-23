2b7628f245aa40d80eb183fe0633b835c15f331b
selvakm75 <30317749+selvakm75@users.noreply.github.com>
2017-07-23 12:20:02 +0530

    Create evennum.c

---
+++ evennum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+int num,m =20,n=43;
+printf("Print even Numbers in a given range m to n");
+for (num = m; num <= n; num++)
+{ 
+if (num % 2 == 0)
+printf ("%d ",&num);
+}}
