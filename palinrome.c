524b2ed3e60bdc1efae5750881e11032be0791b4
selvakm75 <30317749+selvakm75@users.noreply.github.com>
2017-07-23 09:43:07 +0530

    Update palinrome.c

--- palinrome.c
+++ palinrome.c
@@ -5,15 +5,15 @@ int n, reversedInteger = 0, remainder, originalInteger;
 printf("Enter an integer: ");
 scanf("%d", &n);
 originalInteger = n;
-  while( n!=0 )
-{
+while( n!=0 )
+ {
 remainder = n%10;
 reversedInteger = reversedInteger*10 + remainder;
 n /= 10;
- }
+}
 if (originalInteger == reversedInteger)
 printf("%d is a palindrome");
- else
- printf("%d is not a palindrome");
- return 0;
+else
+printf("%d is not a palindrome");
+return 0;
 }
