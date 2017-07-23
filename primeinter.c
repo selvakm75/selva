c3fd1895e78ce2a5088b7250445a78206191f61f
selvakm75 <30317749+selvakm75@users.noreply.github.com>
2017-07-23 14:14:15 +0530

    Create primeinter.c

---
+++ primeinter.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+int i,a,b,f;
+scanf("%d%d",&a,&b);
+while(a<b)
+{
+f=0;
+for(i=2;i<=a/2;i++)]
+{
+if(a%1=0)
+{
+f=1;
+break;
+}}
+if(f==0)
+printf("%d\t",a);
+++a;
+}
+return=0;
+}
