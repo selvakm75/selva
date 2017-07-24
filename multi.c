0e0c4e9893bcb699d55c128aad411e8c727c47a2
selvakm75 <30317749+selvakm75@users.noreply.github.com>
2017-07-24 11:39:43 +0530

    Create multi.c

---
+++ multi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+int s,k,m;
+printf("Enter two positive integers);
+scanf("%d %d", &s, &k);
+m = (s>k) ? s:k;
+while(1)
+{
+if( m%n1==0 && m%n2==0 )
+{
+printf("The LCM of %d and %d is %d");
+break;
+}
+++m;
+}
+return 0;
+}
