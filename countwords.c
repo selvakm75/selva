4368bc5d5cf0ef629424815b458e3120a75610a2
selvakm75 <30317749+selvakm75@users.noreply.github.com>
2017-07-24 15:57:47 +0530

    Update countwords.c

--- countwords.c
+++ countwords.c
@@ -3,7 +3,7 @@
 void main()
 {
 char str[100];
-int i = 0, l = 0, s = 1;
+int i = 0, l = 0, k= 1;
 puts("Enter any string\n");
 gets(str);
 for(i = 0; str[i] !='\0'; i++)
@@ -15,7 +15,7 @@ for(i = 0; i <= l-1; i++)
 {
 if(str[i] == ' ')
 {
-s = s + 1;
+k = k + 1;
 }
 }
 printf("The number of words in the string ");
